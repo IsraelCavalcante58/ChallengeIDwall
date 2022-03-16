f801000024a9e4c62b27455a6925e0787509fa94
israelcavalcante54 <leopontorua@gmail.com>
2022-03-16 16:34:26 -0300

    Arrumando as cores, tamanho e description dos gráficos da Protocolo

--- Javascript/chart.js
+++ Javascript/chart.js
@@ -1,5 +1,6 @@
 var options = {
-    series: [100, 100, 100],
+    series: [100, 125],
+    labels: ['Pré-Processamento', 'Que foram cancelados'],
     chart: {
         width: 500,
         type: 'donut',
@@ -9,27 +10,171 @@ var options = {
         enabled: false
     },
     responsive: [{
-        breakpoint: 480,
+        breakpoint: 1456,
         options: {
             chart: {
-                width: 200
+                width: 400
             },
             legend: {
-                show: true
+                show: false
+            }
+        },
+        breakpoint: 858,
+        options: {
+            chart: {
+                width: 300
+            },
+            legend: {
+                show: false
             }
         }
     }],
-    legend: {
-        position: 'right',
-        offsetY: 0,
-        height: 230,
-    },
+    
+
+
     fill: {
-        colors: ['#1A73E8', '#AD6800', '#6300ff' ],
+        colors: ['#AD6800', '#FAAD14' ],
         opacity: 0.9,
         type: 'solid',
+        
+    },
+    legend: {
+        show: true,
+        showForSingleSeries: false,
+        showForNullSeries: true,
+        showForZeroSeries: true,
+        position: 'right',
+        horizontalAlign: 'center',
+        floating: false,
+        fontSize: '10px',
+        fontFamily: 'Helvetica, Arial',
+        fontWeight: 400,
+        formatter: undefined,
+        inverseOrder: false,
+        width: 150,
+        height: 100,
+        tooltipHoverFormatter: undefined,
+        customLegendItems: [],
+        offsetX: 0,
+        offsetY: 0,
+        labels: {
+            colors: ['#AD6800', '#FAAD14'],
+            useSeriesColors: false
+        },
+        markers: {
+            width: 12,
+            height: 12,
+            strokeWidth: 0,
+            strokeColor: '#fff',
+            fillColors: ['#AD6800', '#FAAD14'],
+            radius: 12,
+            customHTML: undefined,
+            onClick: undefined,
+            offsetX: 0,
+            offsetY: 0
+        },
+        itemMargin: {
+            horizontal: 5,
+            vertical: 0
+        },
+        onItemClick: {
+            toggleDataSeries: true
+        },
+        onItemHover: {
+            highlightDataSeries: true
+        },
+    },
 
-    }
+    
+
+    
+};
+var options2 = {
+    series: [100, 125],
+    labels: ['Em processamento', 'Processados'],
+    chart: {
+        width: 500,
+        type: 'donut',
+        
+    },
+    dataLabels: {
+        enabled: false
+    },
+    responsive: [{
+        breakpoint: 1456,
+        options: {
+            chart: {
+                width: 400
+            },
+            legend: {
+                show: false
+
+            }
+        },
+        breakpoint: 858,
+        options: {
+            chart: {
+                width: 300
+            },
+            legend: {
+                show: false
+            }
+        }
+
+    }],
+    
+
+
+    fill: {
+        colors: ['#0087fd','#00e396' ],
+        opacity: 0.9,
+        type: 'solid',
+        
+    },
+    legend: {
+        show: true,
+        showForSingleSeries: false,
+        showForNullSeries: true,
+        showForZeroSeries: true,
+        position: 'right',
+        horizontalAlign: 'center',
+        floating: false,
+        fontSize: '10px',
+        fontFamily: 'Helvetica, Arial',
+        fontWeight: 400,
+        formatter: undefined,
+        inverseOrder: false,
+        width: 150,
+        height: 100,
+        tooltipHoverFormatter: undefined,
+        customLegendItems: [],
+        offsetX: 0,
+        offsetY: 0,
+        labels: {
+            colors: [],
+            useSeriesColors: false
+        },
+        markers: {
+            width: 12,
+            height: 12,
+            strokeWidth: 0,
+            strokeColor: '#fff',
+            fillColors: ['undefined'],
+            radius: 12,
+            customHTML: undefined,
+            onClick: undefined,
+            offsetX: 0,
+            offsetY: 0
+        },
+
+        onItemClick: {
+            toggleDataSeries: true
+        },
+        onItemHover: {
+            highlightDataSeries: true
+        },
+    },
+    
 
     
 };
@@ -39,7 +184,7 @@ var options = {
 
 var chart = new ApexCharts(document.querySelector("#chart"), options);
 chart.render();
-var chart = new ApexCharts(document.querySelector("#chart2"), options);
+var chart = new ApexCharts(document.querySelector("#chart2"), options2);
 chart.render();
 
 
