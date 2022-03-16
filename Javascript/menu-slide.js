2f3505a2221f7c1b111b47219825aa7747d5032c
israelcavalcante54 <leopontorua@gmail.com>
2022-03-16 13:22:02 -0300

    Adicionando Javascript para o botão Menu mudar de cor quando estiver aberto

--- Javascript/menu-slide.js
+++ Javascript/menu-slide.js
@@ -1,8 +1,14 @@
 window.onload = function(){
     var botaoMenu = document.getElementById('openSidebarMenu');
+    var spinner1 = document.getElementById('spinner1');
+    var spinner2 = document.getElementById('spinner2');
+    var spinner3 = document.getElementById('spinner3');
+    console.log(spinner1);
+    console.log(spinner2);
+    console.log(spinner3);
     botaoMenu.onclick = function () {
-        var botaoSpinner = document.getElementsByClassName('spinner');
-        console.log(botaoSpinner);
-        botaoSpinner.style.color = "black";
+        spinner1.classList.toggle('spinner-inactive')
+        spinner2.classList.toggle('spinner-inactive')
+        spinner3.classList.toggle('spinner-inactive')
     }
 }
